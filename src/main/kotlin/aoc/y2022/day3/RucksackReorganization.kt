5d817fd2b914cc7ce646f4e957c326dab48bc801
kanishk <k.kanishk.s@gmail.com>
2022-12-04 11:20:26 -0800

    Refactoring Day 3 code to use tests instead of main function.

--- src/main/kotlin/aoc/y2022/day3/RucksackReorganization.kt
+++ src/main/kotlin/aoc/y2022/day3/RucksackReorganization.kt
@@ -2,40 +2,42 @@ package aoc.y2022.day3
 
 import java.io.File
 
-fun findPrioritySum(rucksacks: List<String>): Int {
-    return rucksacks.map { rucksack ->
-        val characterIndices = mutableMapOf<Char, Int>()
-        for ((index, item) in rucksack.toCharArray().withIndex()) {
-            if (index >= rucksack.length/2
-                && item in characterIndices
-                && characterIndices[item]!! < rucksack.length/2) {
-                return@map item
-            }
-            characterIndices[item] = index
-        }
-        error("Not happening.")
-    }.sumOf { getPriorityValue(it) }
+/**
+ * Explanation:
+ *   1. Take each string in the rucksacks list and split it into two strings of equal size.
+ *   2. As part of the chunking operation convert the string to the set of characters in the string.
+ *   3. At this point, we have a list where each element of the list is a list containing two sets. We need to find the character common to these two sets.
+ *   4. Reduce each element in the top level list (lists with two sets in them) to a list of characters that are common in the two sets (there is just one).
+ *   5. FlatMap the list of characters into one list and sum their priority levels.
+ */
+fun findPrioritySum(inputPathname: String): Int {
+    val rucksacks = File(inputPathname).readLines()
+    return rucksacks
+        .map { it.chunked(it.length/2) { s -> s.toSet() } }
+        .flatMap { it.reduce { acc, next -> acc.intersect(next) } }
+        .sumOf { priorityValue(it) }
 }
 
-fun findGroupSums(inputRucksacks: List<String>): Int {
-    return inputRucksacks.chunked(3).map { rucksackGroup ->
-        rucksackGroup[0].toCharArray()
-            .intersect(rucksackGroup[1].toCharArray().toSet())
-            .intersect(rucksackGroup[2].toCharArray().toSet())
-            .first()
-    }.sumOf { getPriorityValue(it) }
+/**
+ * Explanation:
+ *   1. Chunk the input list into a list containing lists of 3 strings.
+ *   2. While chunking map the strings to set of characters.
+ *   3. At this point, we have a list where each element of the list is a list containing 3 sets. We need to find the character common to these 3 sets.
+ *   4. Reduce each element in the top level list (lists with 3 sets in them) to a list of characters that are common in the 3 sets (there is just one).
+ *   5. FlatMap the list of characters into one list and sum their priority levels.
+ */
+fun findGroupsSum(inputPathname: String): Int {
+    val rucksacks = File(inputPathname).readLines()
+    return rucksacks
+        .chunked(3) { it.map { str -> str.toSet() }}
+        .flatMap { it.reduce { acc, next -> acc.intersect(next) } }
+        .sumOf { priorityValue(it) }
 }
 
-fun getPriorityValue(character: Char): Int {
+fun priorityValue(character: Char): Int {
     return if (character.isUpperCase()) {
         character - 'A' + 27
     } else {
         character - 'a' + 1
     }
-}
-
-fun main() {
-    val inputRucksacks = File("./src/main/resources/input_day3_rucksackreorganization").readLines()
-    println(findPrioritySum(inputRucksacks))
-    println(findGroupSums(inputRucksacks))
 }
---
+++ src/test/kotlin/aoc/y2022/day3/RucksackReorganizationTest.kt
@@ -0,0 +1,15 @@
+package aoc.y2022.day3
+
+import kotlin.test.Test
+
+class RucksackReorganizationTest {
+    @Test
+    fun `run findGroupsSum`() {
+        println(findGroupsSum("./src/main/resources/input_day3_rucksackreorganization"))
+    }
+
+    @Test
+    fun `run findPrioritySum`() {
+        println(findPrioritySum("./src/main/resources/input_day3_rucksackreorganization"))
+    }
+}
